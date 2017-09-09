b426b1a940c07c83421a17de3c9193c62d8be282
Hrishi Mittal <hrishimittal@gmail.com>
2017-09-09 13:25:35 +0100

    Our new online store

---
+++ app/controllers/charges_controller.rb
@@ -0,0 +1,21 @@
+class ChargesController < ApplicationController
+  def create
+    # Amount in cents
+    @amount = 500
+
+    customer = Stripe::Customer.create(
+      :email => params[:stripeEmail],
+      :source  => params[:stripeToken]
+    )
+
+    charge = Stripe::Charge.create(
+      :customer    => customer.id,
+      :amount      => @amount,
+      :description => 'pack of 5 stickers',
+      :currency    => 'usd'
+    )
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to new_charge_path
+  end
+end
